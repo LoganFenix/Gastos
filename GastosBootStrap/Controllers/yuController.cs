53459ae3fbbdc0649c6c2252b9abaca7cdb96547
rpavon_2 <rpavon_2@RPAVON_2>
2016-06-29 18:37:53 -0500

    probando actualizacion

---
+++ GastosBootStrap/Controllers/yuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GastosBootStrap.Controllers
+{
+    public class yuController : Controller
+    {
+        // GET: yu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
