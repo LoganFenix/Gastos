747f20d05c50b9b80e6aac4b5c1e59e8e44e0cc4
rpavon_2 <rpavon_2@RPAVON_2>
2016-06-29 18:25:37 -0500

    viendo si sirve

---
+++ GastosBootStrap/Controllers/Prueba2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GastosBootStrap.Controllers
+{
+    public class Prueba2Controller : Controller
+    {
+        // GET: Prueba2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
